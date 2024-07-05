358b3e7a4b74587ce467e8432812f832a2a55401
Atharva <atharva.malik@gmail.com>
2024-07-05 12:56:31 +1000

    init

---
+++ Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    public bool isSafe;
+
+    [Space(10)]
+
+    public bool redStartPoint;
+    public bool greenStartPoint;
+    public bool blueStartPoint;
+    public bool yellowStartPoint;
+
+    [Space(10)]
+
+    public bool redEntryPoint;
+    public bool greenEntryPoint;
+    public bool blueEntryPoint;
+    public bool yellowEntryPoint;
+
+    [Space(10)]
+
+    public int redEntryCheckpointNumber;
+    public int greenEntryCheckpointNumber;
+    public int blueEntryCheckpointNumber;
+    public int yellowEntryCheckpointNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
